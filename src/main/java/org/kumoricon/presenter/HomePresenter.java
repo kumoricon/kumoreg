13801ac502826145b66da2fd8bf93867571cad44
Jason Short <jason@sheersky.com>
2016-02-01 19:57:22 -0800

    Added badge type list to home view

---
+++ src/main/java/org/kumoricon/presenter/HomePresenter.java
@@ -0,0 +1,30 @@
+package org.kumoricon.presenter;
+
+import org.kumoricon.model.badge.Badge;
+import org.kumoricon.model.badge.BadgeRepository;
+import org.kumoricon.view.HomeView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@Scope("request")
+public class HomePresenter {
+
+    @Autowired
+    private BadgeRepository badgeRepository;
+
+
+    private HomeView view;
+
+    public void showBadges() {
+        List<Badge> badges = badgeRepository.findByVisibleTrue();
+        view.showBadges(badges);
+    }
+
+    public HomeView getView() { return view; }
+
+    public void setView(HomeView view) { this.view = view; }
+}
