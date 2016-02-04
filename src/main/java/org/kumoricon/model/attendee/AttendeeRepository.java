b34ab293a0be16a49de74d8e00318dce1787985c
Jason Short <jason@sheersky.com>
2016-02-03 20:26:37 -0800

    Added check ins by day to attendee report

--- src/main/java/org/kumoricon/model/attendee/AttendeeRepository.java
+++ src/main/java/org/kumoricon/model/attendee/AttendeeRepository.java
@@ -14,15 +14,6 @@ public interface AttendeeRepository extends JpaRepository<Attendee, Integer> {
     List<Attendee> findByLastNameOrBadgeNumber(String searchString);
     List<Attendee> findByOrder(Order order);
 
-    @Query(value = "select a.badge, count(a) from Attendee a group by a.badge")
-    List<Object[]> findCountPerBadgeType();
-
-    @Query(value = "select a.badge, count(a) from Attendee a where a.checkedIn = true group by a.badge ")
-    List<Object[]> findCountPerBadgeTypeCheckedIn();
-
-    @Query(value = "select a.badge, count(a) from Attendee a where a.checkedIn = false group by a.badge ")
-    List<Object[]> findCountPerBadgeTypeNotCheckedIn();
-
     @Query(value = "select a from Attendee a where a.lastName like ?1% and a.checkedIn = false")
     List<Attendee> findNotCheckedInByLastName(String searchString);
 
@@ -31,4 +22,10 @@ public interface AttendeeRepository extends JpaRepository<Attendee, Integer> {
 
     @Query(value = "SELECT badges.name as Badge, IFNULL(atConCheckedIn.cnt, 0) as AtConCheckedIn, IFNULL(atConNotCheckedIn.cnt, 0) as AtConNotCheckedIn, IFNULL(preRegCheckedIn.cnt, 0) as PreRegCheckedIn, IFNULL(preRegNotCheckedIn.cnt, 0) as PreRegNotCheckedIn FROM badges LEFT OUTER JOIN (SELECT badges.id as subid, COUNT(attendees.checked_in) as cnt FROM badges JOIN attendees ON badges.id = attendees.badge_id WHERE attendees.checked_in = TRUE AND attendees.pre_registered = FALSE GROUP BY badges.id) as atConCheckedIn ON badges.id = atConCheckedIn.subid LEFT OUTER JOIN (SELECT badges.id as subid, COUNT(attendees.id) as cnt FROM badges JOIN attendees ON badges.id = attendees.badge_id WHERE attendees.checked_in = FALSE AND attendees.pre_registered = FALSE GROUP BY badges.id) as atConNotCheckedIn ON badges.id = atConNotCheckedIn.subid LEFT OUTER JOIN (SELECT badges.id as subid, COUNT(attendees.checked_in) as cnt FROM badges JOIN attendees ON badges.id = attendees.badge_id WHERE attendees.checked_in = TRUE AND attendees.pre_registered = TRUE GROUP BY badges.id) as preRegCheckedIn ON badges.id = preRegCheckedIn.subid LEFT OUTER JOIN (SELECT badges.id as subid, COUNT(attendees.id) as cnt FROM badges JOIN attendees ON badges.id = attendees.badge_id WHERE attendees.checked_in = FALSE AND attendees.pre_registered = TRUE GROUP BY badges.id) as preRegNotCheckedIn ON badges.id = preRegNotCheckedIn.subid;", nativeQuery = true)
     List<Object[]> findBadgeCounts();
+
+    @Query(value = "SELECT DATE(check_in_time) as CheckInDate, COUNT(id) AS cnt, SUM(paid_amount) as Amount FROM attendees WHERE checked_in = TRUE AND pre_registered = FALSE GROUP BY CheckInDate ORDER BY CheckInDate;", nativeQuery = true)
+    List<Object[]> findAtConCheckInCountsByDate();
+
+    @Query(value = "SELECT DATE(check_in_time) as CheckInDate, COUNT(id) AS cnt, SUM(paid_amount) as Amount FROM attendees WHERE checked_in = TRUE AND pre_registered = TRUE GROUP BY CheckInDate ORDER BY CheckInDate;", nativeQuery = true)
+    List<Object[]> findPreRegCheckInCountsByDate();
 }
--- src/main/java/org/kumoricon/presenter/report/AttendeeReportPresenter.java
+++ src/main/java/org/kumoricon/presenter/report/AttendeeReportPresenter.java
@@ -23,8 +23,9 @@ public class AttendeeReportPresenter {
     public AttendeeReportView getView() { return view; }
     public void setView(AttendeeReportView view) { this.view = view; }
 
-    public void showReport() {
+    public String getTotalsByBadgeType() {
         StringBuilder sb = new StringBuilder();
+        sb.append("<h2>Check Ins by Badge Type</h2>");
         sb.append("<table border=\"1\"><tr><td>Badge Type</td>");
         sb.append("<td>At-Con Checked In</td><td>At-Con Not Checked In</td>");
         sb.append("<td>Pre Reg Checked In</td><td>Pre Reg Not Checked In</td></tr>");
@@ -39,6 +40,29 @@ public class AttendeeReportPresenter {
             sb.append("</tr>");
         }
         sb.append("</table>");
+        return sb.toString();
+    }
+
+    public void showReport() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getTotalsByBadgeType());
+        sb.append(buildTable("At Con Check Ins By Day", attendeeRepository.findAtConCheckInCountsByDate()));
+        sb.append(buildTable("Pre Reg Check Ins By Day", attendeeRepository.findPreRegCheckInCountsByDate()));
         view.afterSuccessfulFetch(sb.toString());
     }
+
+    private static String buildTable(String title, List<Object[]> data) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<h2>" + title + "</h2>");
+        sb.append("<table border=\"1\"><tr><td>Date</td><td> Checked In</td></tr>");
+        for (Object[] line : data) {
+            sb.append("<tr>");
+            sb.append("<td>" + line[0].toString() + "</td>");
+            sb.append("<td align=\"right\">" + line[1].toString() + "</td>");
+            sb.append("</tr>");
+        }
+        sb.append("</table>");
+        return sb.toString();
+    }
+
 }
