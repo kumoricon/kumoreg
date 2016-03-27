311d8a46e8d6892a32fea13288f97481d78da4e8
Jason Short <jason@sheersky.com>
2016-03-27 10:35:12 -0700

    Added configuration to application.properties to enable/disable printing badges from server

--- src/main/java/org/kumoricon/presenter/utility/PrintBadgePresenter.java
+++ src/main/java/org/kumoricon/presenter/utility/PrintBadgePresenter.java
@@ -11,6 +11,7 @@ import org.kumoricon.view.attendee.PrintBadgeWindow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
@@ -29,6 +30,10 @@ public class PrintBadgePresenter implements PrintBadgeHandler {
     @Autowired
     private BadgePrintService badgePrintService;
 
+
+    @Value("${kumoreg.printing.enablePrintingFromServer}")
+    private Boolean enablePrintingFromServer;
+
     public PrintBadgePresenter() {}
 
     @Override
@@ -38,8 +43,7 @@ public class PrintBadgePresenter implements PrintBadgeHandler {
         attendeeList.add(attendeeFactory.generateDemoAttendee());
         attendeeList.add(attendeeFactory.generateYouthAttendee());
         attendeeList.add(attendeeFactory.generateChildAttendee());
-        // Todo: Fix this
-//        printBadgeWindow = new PrintBadgeWindow(this, attendeeList);
+
         printBadges((BaseView)view, attendeeList);
         view.showPrintBadgeWindow(attendeeList);
     }
@@ -65,12 +69,17 @@ public class PrintBadgePresenter implements PrintBadgeHandler {
 
     private void printBadges(BaseView view, List<Attendee> attendeeList) {
         BadgeGenerator badgeGenerator = new BadgeGenerator(attendeeList);
-        try {
-            badgePrintService.printDocument(badgeGenerator.getStream(), "Test");
-        } catch(PrintException e) {
-            view.notifyError("Error printing. No printers found? More information in server logs");
-            log.error(String.format("Error printing badge for %s: %s",
-                    view.getCurrentUser().getUsername(), e.getMessage()), e);
+
+        if (enablePrintingFromServer != null && enablePrintingFromServer) {
+            try {
+                badgePrintService.printDocument(badgeGenerator.getStream(), "Test");
+            } catch (PrintException e) {
+                view.notifyError("Error printing. No printers found? More information in server logs");
+                log.error(String.format("Error printing badge for %s: %s",
+                        view.getCurrentUser().getUsername(), e.getMessage()), e);
+            }
+        } else {
+            view.notify("Printing from server not enabled. Select \"Show Badges in Browser\".");
         }
     }
 }
