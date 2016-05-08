7e62dbdde6e9f0d71f0e4a5cdbf58731686b5fe6
Jason Short <jason@sheersky.com>
2016-05-08 11:37:26 -0700

    Simplified AttendeePreRegDetailForm

--- src/main/java/org/kumoricon/site/attendee/form/AttendeePreRegDetailForm.java
+++ src/main/java/org/kumoricon/site/attendee/form/AttendeePreRegDetailForm.java
@@ -1,37 +1,19 @@
 package org.kumoricon.site.attendee.form;
 
 import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.HorizontalLayout;
 
 public class AttendeePreRegDetailForm extends AttendeeDetailForm {
     // Subclass the attendee detail form and hide the "parental consent form received" checkbox
-    // so that it can be displayed down near the bottom of the form instead
+    // so that it can be displayed down near the bottom of the form instead, and the "checked in"
+    // checkbox because it doesn't make any sense here
     public AttendeePreRegDetailForm() {
         super();
     }
 
     @Override
-    protected FormLayout buildParentInfo() {
-        FormLayout f = new FormLayout();
-        f.setCaption("Parent Information");
-        f.setMargin(false);
-
-        HorizontalLayout checkBoxes = new HorizontalLayout();
-        checkBoxes.setMargin(false);
-        checkBoxes.setSpacing(true);
-        checkBoxes.addComponent(parentIsEmergencyContact);
-
-        f.addComponent(parentFullName);
-        f.addComponent(parentPhone);
-        f.addComponent(checkBoxes);
-
-        return f;
-    }
-
-    @Override
     protected FormLayout buildCheckedIn() {
         FormLayout f = new FormLayout();
-        // Don't show the checked in checkbox on the check in form
+        // Don't show the consent form or checked in checkbox here
         return f;
     }
 }
